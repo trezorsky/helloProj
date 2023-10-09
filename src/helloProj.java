7fba1c88981b93e591b7e09839003532f5a75de8
trezorsky <130697823+trezorsky@users.noreply.github.com>
2023-10-09 18:43:50 +0300

    module2_q3

--- src/helloProj.java
+++ src/helloProj.java
@@ -1,15 +1,33 @@
-import java.util.Scanner;
+import java.util.Random;
 
 public class helloProj {
 
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
+        int[] array = new int[20];
+        Random random = new Random();
 
-        System.out.println("Как тебя зовут?");
-        String name = scanner.nextLine();
-        System.out.println("Привет, " + name + "!");
-        scanner.close();
+        // от 1 до 15
+        for (int i = 0; i < array.length; i++) {
+            array[i] = random.nextInt(15) + 1;
+        }
 
+        System.out.println("Содержимое массива: ");
+        for (int i : array) {
+            System.out.print(i + " ");
+        }
+
+        System.out.println("\nЗначения, встречающиеся больше одного раза:");
+        for (int i = 1; i <= 15; i++) {
+            int count = 0; // Счетчик встречаемости числа
+            for (int j = 0; j < array.length; j++) {
+                if (array[j] == i) {
+                    count++;
+                }
+            }
+            if (count > 1) {
+                System.out.println("число '" + i + "' встречается " + count + " раза");
+            }
+        }
     }
 }
