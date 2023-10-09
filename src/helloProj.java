d83e7706ba45b9d83bb3cb81d9df3454059130c1
trezorsky <130697823+trezorsky@users.noreply.github.com>
2023-10-09 19:21:58 +0300

    module2_q4.3

--- src/helloProj.java
+++ src/helloProj.java
@@ -5,14 +5,19 @@ public class helloProj {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Введите строку: ");
-        String inputString = scanner.nextLine();
+        System.out.print("Введите дату в формате dd.mm.yyyy: ");
+        String dateString = scanner.nextLine();
 
-        // Заменяем слова "кака" и "бяка" на "вырезано цензурой"
-        String censoredString = inputString.replaceAll("(.*)кака(.*)|(.*)бяка(.*)", "вырезано цензурой");
-
-        System.out.println("Результат замены:");
-        System.out.println(censoredString);
+        String[] parts = dateString.split("\\.");
+        if (parts.length == 3) {
+            String year = parts[2];
+            String month = parts[1];
+            String day = parts[0];
+            String formattedDate = year + "-" + month + "-" + day;
+            System.out.println("Преобразованная дата: " + formattedDate);
+        } else {
+            System.out.println("Некорректный формат даты.");
+        }
 
         scanner.close();
     }
