c12e742473a010d3edba2778d390acb97376b39e
trezorsky <130697823+trezorsky@users.noreply.github.com>
2023-10-09 19:17:32 +0300

    module2_q4.2

--- src/helloProj.java
+++ src/helloProj.java
@@ -8,23 +8,12 @@ public class helloProj {
         System.out.print("Введите строку: ");
         String inputString = scanner.nextLine();
 
-        System.out.print("Введите подстроку: ");
-        String substring = scanner.nextLine();
+        // Заменяем слова "кака" и "бяка" на "вырезано цензурой"
+        String censoredString = inputString.replaceAll("(.*)кака(.*)|(.*)бяка(.*)", "вырезано цензурой");
 
-        int count = countSubstr(inputString, substring);
-        System.out.println("Подстрока встречается " + count + " раз(а) в строке.");
+        System.out.println("Результат замены:");
+        System.out.println(censoredString);
 
         scanner.close();
-    }
-
-    // Метод для подсчета количества вхождений подстроки в строку
-    public static int countSubstr(String input, String substring) {
-        int count = 0;
-        int index = input.indexOf(substring);
-        while (index != -1) {
-            count++;
-            index = input.indexOf(substring, index + 1);
-        }
-        return count;
     }
 }
