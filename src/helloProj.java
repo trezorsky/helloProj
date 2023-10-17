5f42ea7534e1ad78d1e8a88d2444b0e5a0d023b2
trezorsky <130697823+trezorsky@users.noreply.github.com>
2023-10-17 17:47:33 +0300

    module2_q6

--- src/helloProj.java
+++ src/helloProj.java
@@ -1,15 +1,68 @@
 import java.util.Scanner;
 
-public class helloProj {
+    public class helloProj {
 
-    public static void main(String[] args) {
+        public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
+            Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Как тебя зовут?");
-        String name = scanner.nextLine();
-        System.out.println("Привет, " + name + "!");
-        scanner.close();
+            System.out.print("Введите имя первого пользователя: ");
+            String name1 = scanner.nextLine();
 
+            System.out.print("Введите возраст первого пользователя: ");
+            int age1 = scanner.nextInt();
+            scanner.nextLine(); // следующая строка
+
+            User user1 = new User(name1, age1);
+
+            System.out.print("Введите имя второго пользователя: ");
+            String name2 = scanner.nextLine();
+
+            System.out.print("Введите возраст второго пользователя: ");
+            int age2 = scanner.nextInt();
+
+            User user2 = new User(name2, age2);
+
+            System.out.println("Первый пользователь: " + user1);
+            System.out.println("Второй пользователь: " + user2);
+
+            if (user1.getAge() < user2.getAge()) {
+                System.out.println("Пользователь с наименьшим возрастом: " + user1);
+            } else {
+                System.out.println("Пользователь с наименьшим возрастом: " + user2);
+            }
+        }
     }
-}
+
+    class User {
+        private String name;
+        private Integer age;
+
+        public User(String name, Integer age) {
+            this.name = name;
+            this.age = age;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public Integer getAge() {
+            return age;
+        }
+
+        public void setAge(Integer age) {
+            this.age = age;
+        }
+
+        @Override
+        public String toString() {
+            return name + ", возраст " + age + " лет";
+        }
+    }
+
+
