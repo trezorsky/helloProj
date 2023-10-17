fa8efc2ab3970fe58af02ef16039f7f9a2990e82
trezorsky <130697823+trezorsky@users.noreply.github.com>
2023-10-17 18:22:38 +0300

    module3_q7

--- src/helloProj.java
+++ src/helloProj.java
@@ -1,68 +1,69 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 import java.util.Scanner;
 
-    public class helloProj {
+public class helloProj {
+    public static void main(String[] args) {
 
-        public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        List<User> userList = new ArrayList<>();
 
-            Scanner scanner = new Scanner(System.in);
+        for (int i = 0; i < 5; i++) {
+            System.out.print("Введите имя пользователя: ");
+            String name = scanner.nextLine();
 
-            System.out.print("Введите имя первого пользователя: ");
-            String name1 = scanner.nextLine();
+            System.out.print("Введите возраст пользователя: ");
+            int age = scanner.nextInt();
+            scanner.nextLine();
 
-            System.out.print("Введите возраст первого пользователя: ");
-            int age1 = scanner.nextInt();
-            scanner.nextLine(); // следующая строка
+            User user = new User(name, age);
+            userList.add(user);
+        }
 
-            User user1 = new User(name1, age1);
-
-            System.out.print("Введите имя второго пользователя: ");
-            String name2 = scanner.nextLine();
-
-            System.out.print("Введите возраст второго пользователя: ");
-            int age2 = scanner.nextInt();
-
-            User user2 = new User(name2, age2);
-
-            System.out.println("Первый пользователь: " + user1);
-            System.out.println("Второй пользователь: " + user2);
-
-            if (user1.getAge() < user2.getAge()) {
-                System.out.println("Пользователь с наименьшим возрастом: " + user1);
-            } else {
-                System.out.println("Пользователь с наименьшим возрастом: " + user2);
+        // Сортировка списка пользователей по возрастанию возраста
+        Collections.sort(userList, new Comparator<User>() {
+            @Override
+            public int compare(User user1, User user2) {
+                return user1.getAge().compareTo(user2.getAge());
             }
+        });
+
+        // Вывод отсортированного списка пользователей
+        for (User user : userList) {
+            System.out.println(user.toString());
         }
     }
+}
 
-    class User {
-        private String name;
-        private Integer age;
+class User {
+    private String name;
+    private Integer age;
 
-        public User(String name, Integer age) {
-            this.name = name;
-            this.age = age;
-        }
-
-        public String getName() {
-            return name;
-        }
-
-        public void setName(String name) {
-            this.name = name;
-        }
-
-        public Integer getAge() {
-            return age;
-        }
-
-        public void setAge(Integer age) {
-            this.age = age;
-        }
-
-        @Override
-        public String toString() {
-            return name + ", возраст " + age + " лет";
-        }
+    public User(String name, Integer age) {
+        this.name = name;
+        this.age = age;
     }
 
+    public String getName() {
+        return name;
+    }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return name + ", возраст " + age + " лет";
+    }
+}
