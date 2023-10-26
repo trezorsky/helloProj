6435e40e4f71f429781558db7843da92fb786aec
trezorsky <130697823+trezorsky@users.noreply.github.com>
2023-10-26 21:46:53 +0300

    module4_q9

--- src/module4_q9/Learner.java
+++ src/module4_q9/Learner.java
@@ -2,4 +2,5 @@ package module4_q9;
 
 public interface Learner {
     void learn();
+
 }
--- src/module4_q9/Student.java
+++ src/module4_q9/Student.java
@@ -1,13 +1,21 @@
 package module4_q9;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.stream.IntStream;
 
-public class Student implements Learner{
+public class Student implements Learner {
 
     @Override
     public void learn() {
+
+        Date date = new Date();
+        SimpleDateFormat formatDate = new SimpleDateFormat("HH:mm:ss");
+        System.out.println("Текущее системное время: " + formatDate.format(date));
+
         System.out.println("Я учусь. .zZ");
         IntStream.range(0, 1000000).forEach(i -> Math.log(i));
         System.out.println("Я закончил учиться");
+
     }
 }
